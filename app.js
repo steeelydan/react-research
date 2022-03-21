5d04ee996225b25ca9c4dbafbfde223ee44bef4b
Steeely Dan <steeelydan@posteo.de>
2022-03-21 23:32:50 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log('React', React);
+console.log('ReactDOM', ReactDOM);
+
+const App = React.createElement('div', null, 'Hello React');
+
+ReactDOM.render(App, document.getElementById('root'));
